5dc00c1facc89e4be450c0727e9f0e08b0f4b702
Lukas Breit <l.breit@outlook.de>
2015-06-29 17:25:39 +0200

    Lösung eines Problems mit Git

--- MyBudgetManager/src/BudgetPlan.java
+++ MyBudgetManager/src/BudgetPlan.java
@@ -20,7 +20,7 @@
 public class BudgetPlan {
 	public static void main(String[] args) {
 		BudgetPlanModel budget = new BudgetPlanModel(); // Modell
-		new BudgetPlanGUI(budget); // View und Controller
+		new BudgetPlanGUI(budget); //View und Controller
 	}
 
 }
